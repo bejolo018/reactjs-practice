fdd0cbb97ed7eaae64973e44ee8099bff5523c6d
bejolo018 <bejolo018@gmail.com>
2020-05-28 23:35:53 -0500

    created a few components, working on items.

--- todo-practice/src/App.js
+++ todo-practice/src/App.js
@@ -1,12 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Todos from './components/Todos';
 import './App.css';
 
-function App() {
+class App extends Component {
+  state ={
+    todos:[
+      {
+        id: 1,
+        title: 'Ghost In The Shell',
+        completed: true
+      },
+      {
+        id: 2,
+        title: 'Your Name',
+        completed: true
+      },
+      {
+        id: 3,
+        title: 'Promare',
+        completed: false
+      },
+    ]
+  }
+
+
+render() {
   return (
     <div className="App">
-123
+        <Todos todos={this.state.todos} />
     </div>
   );
 }
-
+}
 export default App;
