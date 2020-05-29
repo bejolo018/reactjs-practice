b525918370f47e92b2a8ec759bbdd33e9da3c827
bejolo018 <bejolo018@gmail.com>
2020-05-28 23:46:02 -0500

    able to toggle todo items

--- todo-practice/src/App.js
+++ todo-practice/src/App.js
@@ -8,12 +8,12 @@ class App extends Component {
       {
         id: 1,
         title: 'Ghost In The Shell',
-        completed: true
+        completed: false
       },
       {
         id: 2,
         title: 'Your Name',
-        completed: true
+        completed: false
       },
       {
         id: 3,
@@ -23,11 +23,19 @@ class App extends Component {
     ]
   }
 
+  markComplete = (id) => {
+        this.setState({ todos: this.state.todos.map(todo => {
+          if (todo.id === id){
+            todo.completed = !todo.completed
+          }
+          return todo
+        }) });
+  }
 
 render() {
   return (
     <div className="App">
-        <Todos todos={this.state.todos} />
+        <Todos todos={this.state.todos} markComplete={this.markComplete}/>
     </div>
   );
 }
