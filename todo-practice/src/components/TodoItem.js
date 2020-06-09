0b08d618ae173a5dbb06a4f6b107a589e58c06aa
Benjamin Lopez <47269255+bejolo018@users.noreply.github.com>
2020-06-09 11:31:59 -0500

    created delete function, started header

--- todo-practice/src/components/TodoItem.js
+++ todo-practice/src/components/TodoItem.js
@@ -20,14 +20,27 @@ export class TodoItem extends Component {
                 <input type="checkbox" onChange={this.props.markComplete.bind
                 (this, id)} /> { '  ' }
                 {title}
+                <button onClick={this.props.delTodo.bind(this, id)}style={bynStyle}>x</button>
                 </p>
             </div>
         )
     }
 }
 
+
+//PropTypes
 TodoItem.propTypes = {
     todo: PropTypes.object.isRequired
+}
+
+const bynStyle = {
+    background: '#ff0000',
+    color: 'fff',
+    border: 'none',
+    padding: '5px 9px',
+    borderRadius: '50%',
+    cursor: 'pointer',
+    float: 'right'
 }
 
 export default TodoItem
