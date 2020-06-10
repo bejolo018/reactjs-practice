388868263e6ab2063c39b6db51c55b25cfbe83cd
Benjamin Lopez <47269255+bejolo018@users.noreply.github.com>
2020-06-10 10:43:06 -0500

    created header, started addtodo function

--- todo-practice/src/App.js
+++ todo-practice/src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import Todos from './components/Todos';
+import Header from './components/layout/Header'
+import AddTodo from './components/AddTodo'
 import './App.css';
 
 class App extends Component {
@@ -41,8 +43,12 @@ delTodo = (id) => {
 render() {
   return (
     <div className="App">
+      <div className="container">
+      <Header />
+        <AddTodo />
         <Todos todos={this.state.todos} markComplete={this.markComplete}
         delTodo={this.delTodo}/>
+      </div>
     </div>
   );
 }
