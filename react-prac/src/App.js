7b3c1ac8b9f1200ea572b3cd3e00a0667d63e10f
Benjamin Lopez <47269255+bejolo018@users.noreply.github.com>
2020-08-10 17:02:15 -0500

    git ignore for counter example

--- react-prac/src/App.js
+++ react-prac/src/App.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import './App.css';
 import HelloWorld from './Components/HelloWorld';
-import CounterExample from './Components/CounterExample';
+
 
 function App() {
   return (
     <div>
-      {/* <HelloWorld name="Dumb"/> */}
-      <CounterExample />
+      <HelloWorld name="Dumb"/>
     </div>
   );
 }
