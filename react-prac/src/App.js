b06e01173d84d05a0690b893237136bf2784279f
Benjamin Lopez <47269255+bejolo018@users.noreply.github.com>
2020-08-11 16:21:52 -0500

    set up tailwind for easieer styling, created header and footer components

--- react-prac/src/App.js
+++ react-prac/src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
-import './App.css';
-import HelloWorld from './Components/HelloWorld';
 
+import HelloWorld from './Components/HelloWorld';
+import Header from './Components/Header';
+import Footer from './Components/Footer'
 
 function App() {
   return (
     <div>
+      <Header />
       <HelloWorld name="Dumb"/>
+      <Footer />
     </div>
   );
 }
