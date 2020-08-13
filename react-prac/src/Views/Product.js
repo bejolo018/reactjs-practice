23f27d9ed7936b47add456855d0e9a643aa7fe57
Benjamin Lopez <47269255+bejolo018@users.noreply.github.com>
2020-08-13 18:24:54 -0500

    Added loading animation for while the page loads, modified product.js slightly to do it

--- react-prac/src/Views/Product.js
+++ react-prac/src/Views/Product.js
@@ -1,37 +1,55 @@
 import React, {useState, useEffect} from 'react'
 import axios from 'axios'
 import {useParams } from 'react-router-dom'
+import Loader from '../Components/Loader'
 
 function Product() {
     const { id } = useParams()
 
     const url = `https://5f35c1445b91f60016ca50c1.mockapi.io/products/${id}`
-    const [product, setProduct] = useState(null)
+    const [product, setProduct] = useState({
+        loading: false,
+        data: null
+    })
+
+    let content = null
 
     useEffect(() =>{
+        setProduct({
+            loading: true,
+            data: null
+        })
         axios.get(url)
         .then(response => {
-            setProduct(response.data)
+            setProduct({
+                loading: false,
+                data: response.data
+            })
         })
     }, [url])
 
-    if(product){
+    if(product.loading){
+        content = <Loader />
+    }
+
+    if(product.data){
         return (
+        content =
             <div>
                 <h1 className="text-2xl font-bold mb-3">
-                    {product.name}
+                    {product.data.name}
                 </h1>
                 <div>
                     <img
-                        src={product.images}
-                        alt={product.name}
+                        src={product.data.images}
+                        alt={product.data.name}
                     />
                 </div>
                 <div className="font-bold text-xl mb-3">
-                    $ {product.price}
+                    $ {product.data.price}
                 </div>
                 <div>
-                    {product.description}
+                    {product.data.description}
                 </div>
             </div>
         )
@@ -39,7 +57,7 @@ function Product() {
 
     return(
         <div>
-
+            {content}
         </div>
     )
 }
