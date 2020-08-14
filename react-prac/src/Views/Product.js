29461c2c382cbb36f49e7b0dfa7d6baade2a3a50
Benjamin Lopez <47269255+bejolo018@users.noreply.github.com>
2020-08-13 19:03:16 -0500

    added error functionality

--- react-prac/src/Views/Product.js
+++ react-prac/src/Views/Product.js
@@ -1,6 +1,6 @@
 import React, {useState, useEffect} from 'react'
 import axios from 'axios'
-import {useParams } from 'react-router-dom'
+import { useParams } from 'react-router-dom'
 import Loader from '../Components/Loader'
 
 function Product() {
@@ -9,7 +9,8 @@ function Product() {
     const url = `https://5f35c1445b91f60016ca50c1.mockapi.io/products/${id}`
     const [product, setProduct] = useState({
         loading: false,
-        data: null
+        data: null,
+        error: false
     })
 
     let content = null
@@ -17,19 +18,32 @@ function Product() {
     useEffect(() =>{
         setProduct({
             loading: true,
-            data: null
+            data: null,
+            error: false
         })
         axios.get(url)
         .then(response => {
             setProduct({
                 loading: false,
-                data: response.data
+                data: response.data,
+                error: false
+            })
+        })
+        .catch(() =>{
+            setProduct({
+                loading: false,
+                data: null,
+                error: true
             })
         })
     }, [url])
 
     if(product.loading){
         content = <Loader />
+    }
+
+    if(product.error){
+        content = <h1>There was an error please refresh or try again later</h1>
     }
 
     if(product.data){
