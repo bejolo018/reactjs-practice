66c107a5c28a1f90cb1970d46f68c78c4ed123a2
Benjamin Lopez <47269255+bejolo018@users.noreply.github.com>
2020-08-20 22:08:13 -0500

    created new react app, cleaned up unnecessary files and got to work adding the api

---
+++ react-weather/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const api = {
+  key: "f5a633ddfb015dc2023f6a666307074b",
+  base: "https://api.openweathermap.org/data/2.5/"
+
+}
+
+function App() {
+  return (
+    <div>
+      
+    </div>
+  );
+}
+
+export default App;
