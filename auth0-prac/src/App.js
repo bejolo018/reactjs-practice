eb02a39274edc26462f1205559875bb78ddf67bb
Benjamin Lopez <47269255+bejolo018@users.noreply.github.com>
2020-09-24 17:26:24 -0500

    finished learning how to authenticate with auth0, this is aweseome! incredibly easy authentication using auth0s hooks, will probably lean towards this for future authentication

--- auth0-prac/src/App.js
+++ auth0-prac/src/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import './App.css';
 import LoginButton from './Components/LoginButton'
+import LogoutButton from './Components/LogoutButton';
+import { Profile } from './Components/Profile';
+import { useAuth0 } from '@auth0/auth0-react'
 
 function App() {
+  const { isLoading } = useAuth0()
+
+  if(isLoading) return <div>Loading...</div>
+
   return (
     <div>
       <LoginButton />
+      <LogoutButton />
+      <Profile />
     </div>
   );
 }
