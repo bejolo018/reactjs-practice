78a3f89ad0f96cbe87c6f8cd8d2a0cfaa089e460
Benjamin Lopez <47269255+bejolo018@users.noreply.github.com>
2020-10-14 23:11:45 -0500

    want to display data from state in the nav, going to implement context api

--- contextapi-prac/src/App.js
+++ contextapi-prac/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
 import MovieList from './Components/MovieList'
+import Nav from './Components/Nav'
 
 function App() {
   return (
     <div className="App">
+      <Nav />
       <MovieList />
     </div>
   );
---
+++ contextapi-prac/src/Components/Nav.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Nav = () => {
+    return (
+        <div>
+            <nav>
+                <h3>Name</h3>
+                <p> List of movies:</p>
+            </nav>
+
+        </div>
+    )
+}
+
+export default Nav
